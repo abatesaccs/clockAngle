14bb55149bfe2481de5c7fe8aba8e1150d874aa4
Andrew Bates and Trevor Mackin <andrew@>
2019-12-09 13:42:24 -0800

    add clock angle class and prompt user for time input

---
+++ ClockAngle.cs
@@ -0,0 +1,22 @@
+using System;
+
+class ClockAngle 
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter an hour");
+        int hourInput = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter minutes");
+        int minInput = int.Parse(Console.ReadLine());
+    }
+
+    static void convertHours(int hour)
+    {
+
+    }
+
+    static void convertMinutes(int min)
+    {
+        
+    }
+}
